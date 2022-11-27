ca5603a15cca508d713d7ff754fe9f4e11dde9f7
IwamotoKakeru <kganpipi@gmail.com>
2022-11-27 15:15:29 +0900

    Clipped wool wouldn't collide

--- Assets/Scripts/ShaverCtrl.cs
+++ Assets/Scripts/ShaverCtrl.cs
@@ -6,13 +6,19 @@ public class ShaverCtrl : MonoBehaviour
 {
     private float timeToDestroy = 0.2f;
     Rigidbody collisionRb = null;
+    BoxCollider collisionCol = null;
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         if(collisionInfo.gameObject.CompareTag("wool")){
             VibrationCtrl.ShortVibration();
+
             collisionRb = collisionInfo.gameObject.GetComponent<Rigidbody>();
             collisionRb.useGravity = true;
+
+            collisionCol = collisionInfo.gameObject.GetComponent<BoxCollider>();
+            collisionCol.isTrigger = true;
+
             Destroy(collisionInfo.gameObject,timeToDestroy);
         }
     }
