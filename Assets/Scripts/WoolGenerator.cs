bd808e8573bb887b744f1fca51bd4d65e7e06a97
IwamotoKakeru <kganpipi@gmail.com>
2022-11-27 17:20:53 +0900

    random wool generate and add layer

---
+++ Assets/Scripts/WoolGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoolGenerator : MonoBehaviour
+{
+    public GameObject wool = null;
+
+    private float lowerRange = -0.5f,higherRange = 0.5f;
+    private int numOfGeneratedWool = 100;
+    private Vector3 generatePos;
+
+    void Start()
+    {
+        RandomInstantiate();
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("wool")){
+
+        }      
+    }
+
+    void RandomInstantiate(){
+        for(int i=0;i<numOfGeneratedWool;i++){
+            float diffX = Random.Range(lowerRange,higherRange);
+            float diffY = Random.Range(lowerRange,higherRange);
+            float diffZ = Random.Range(lowerRange,higherRange);
+
+            generatePos = this.transform.position +new Vector3(diffX,diffY,diffZ);
+
+            Instantiate(wool,generatePos,Quaternion.identity);
+        }
+    }
+
+}
