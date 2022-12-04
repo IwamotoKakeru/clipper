3f1a3cfe9480214cc85a237b4500134c2705e59d
IwamotoKakeru <kganpipi@gmail.com>
2022-12-05 00:48:00 +0900

    remove unused function

--- Assets/Scripts/WoolGenerator.cs
+++ Assets/Scripts/WoolGenerator.cs
@@ -50,30 +50,6 @@ public class WoolGenerator : MonoBehaviour
         GenerateOnPlane();
     }
 
-    void RandomInstantiate(){
-        for(int i=0;i<numOfGeneratedWool;i++){
-            float diffX = Random.Range(lowerRange,higherRange);
-            float diffY = Random.Range(lowerRange,higherRange);
-            float diffZ = Random.Range(lowerRange,higherRange);
-
-            generatePos = this.transform.position +new Vector3(diffX,diffY,diffZ);
-
-            Instantiate(wool,generatePos,Quaternion.identity,this.transform);
-
-        }
-    }
-
-    void GenerateOnTheVertics(){
-        woolMesh = GetComponent<MeshFilter>().mesh;
-        List<Vector3> vertices = new List<Vector3>();
-        vertices.AddRange(woolMesh.vertices);
-        Transform targetTransform = wool.transform;
-        for(int i=0;i<vertices.Count;i++){
-            vertices[i]= this.transform.TransformPoint(vertices[i]);
-            Instantiate(wool,vertices[i],Quaternion.identity,this.transform);
-        }
-
-    } 
     void GenerateOnPlane(){ 
         woolMesh = GetComponent<MeshFilter>().mesh;
 
