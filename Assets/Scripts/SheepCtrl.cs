14a4414e272e34dcc1e5eaccee20c03c29f7c209
IwamotoKakeru <kganpipi@gmail.com>
2022-12-16 03:17:01 +0900

    Display wool num

--- Assets/Scripts/SheepCtrl.cs
+++ Assets/Scripts/SheepCtrl.cs
@@ -8,17 +8,23 @@ public class SheepCtrl : MonoBehaviour
     public int HP { get; private set; }
 
     private GameObject[] wools;
+    private int maxWoolNum;
 
+    private SheepTextCtrl sheepText;
 
     void Start()
     {
         HP = 4;
+        sheepText = GameObject.Find("SheepText").GetComponent<SheepTextCtrl>();
+        wools = GameObject.FindGameObjectsWithTag("wool");
+        maxWoolNum = wools.Length;
     }
 
     void Update()
     {
         wools = GameObject.FindGameObjectsWithTag("wool");
         Debug.Log(wools.Length);
+        sheepText.SetText(wools.Length.ToString() + "/" + maxWoolNum);
     }
 
     public void Damage(int damageAmount)
--- Assets/Scripts/SheepTextCtrl.cs
+++ Assets/Scripts/SheepTextCtrl.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SheepTextCtrl : MonoBehaviour
 {
     private RectTransform sheepRectTfm;
+    private Text text;
+
+    public void SetText(string str)
+    {
+        text.text = str;
+    }
     void Start()
     {
+        text = GetComponent<Text>();
         sheepRectTfm = GetComponent<RectTransform>();
     }
 
