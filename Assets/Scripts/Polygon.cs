5beab7f395a8be4952df2c1c823adf27ad901671
IwamotoKakeru <kganpipi@gmail.com>
2023-10-30 22:16:16 +0900

    Seperate Files

---
+++ Assets/Scripts/Polygon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Polygon
+{
+    private Vector3[] polyVertices = new Vector3[3];
+    public Vector3[] PolyVertices
+    {
+        get => polyVertices;
+    }
+
+    public Vector3 CrossVector { get; private set; }
+    public float Size { get; private set; }
+    public Vector3 CenterPos { get; private set; }
+
+    public Polygon(Vector3[] recPolyVertices)
+    {
+        polyVertices = recPolyVertices;
+
+        CrossVector = Vector3.Cross(
+            polyVertices[1] - polyVertices[0],
+            polyVertices[2] - polyVertices[0]
+        );
+
+        Size = CrossVector.magnitude / 2;
+
+        CenterPos = (polyVertices[0] + polyVertices[1] + polyVertices[2]) / 3;
+    }
+
+    public Vector3 GetRandomSurfacePos()
+    {
+        //空間ベクトルの点の存在範囲より三角形上のランダムな座標を得る
+        Vector3 RandomSurfacePos;
+
+        float s = Random.value;
+        float t = Random.value;
+
+        if (s + t > 1f)
+        {
+            s = 1f - s;
+            t = 1f - t;
+        }
+        float u = 1f - s - t;
+
+        RandomSurfacePos = s * polyVertices[0] + t * polyVertices[1] + u * polyVertices[2];
+
+        return RandomSurfacePos;
+    }
+}
+
--- Assets/Scripts/WoolGenerator.cs
+++ Assets/Scripts/WoolGenerator.cs
@@ -3,53 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Polygon
-{
-    private Vector3[] polyVertices = new Vector3[3];
-    public Vector3[] PolyVertices
-    {
-        get => polyVertices;
-    }
-
-    public Vector3 CrossVector { get; private set; }
-    public float Size { get; private set; }
-    public Vector3 CenterPos { get; private set; }
-
-    public Polygon(Vector3[] recPolyVertices)
-    {
-        polyVertices = recPolyVertices;
-
-        CrossVector = Vector3.Cross(
-            polyVertices[1] - polyVertices[0],
-            polyVertices[2] - polyVertices[0]
-        );
-
-        Size = CrossVector.magnitude / 2;
-
-        CenterPos = (polyVertices[0] + polyVertices[1] + polyVertices[2]) / 3;
-    }
-
-    public Vector3 GetRandomSurfacePos()
-    {
-        //空間ベクトルの点の存在範囲より三角形上のランダムな座標を得る
-        Vector3 RandomSurfacePos;
-
-        float s = Random.value;
-        float t = Random.value;
-
-        if (s + t > 1f)
-        {
-            s = 1f - s;
-            t = 1f - t;
-        }
-        float u = 1f - s - t;
-
-        RandomSurfacePos = s * polyVertices[0] + t * polyVertices[1] + u * polyVertices[2];
-
-        return RandomSurfacePos;
-    }
-}
-
 public class WoolGenerator : MonoBehaviour
 {
     public GameObject wool = null;
@@ -78,15 +31,14 @@ public class WoolGenerator : MonoBehaviour
 
         List<Polygon> polygons = new List<Polygon>();
 
+        // 3つの頂点ごとにポリゴンインスタンスの生成を行う
         for (int i = 0; i < verticesIndex.Count; i = i + 3)
         {
-
             Vector3[] polyVertices ={
                 vertices[verticesIndex[i]],
                 vertices[verticesIndex[i+1]],
                 vertices[verticesIndex[i+2]]
             };
-
             polygons.Add(new Polygon(polyVertices));
         }
 
